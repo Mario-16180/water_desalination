e11bc28082b67716fade673073d458af1280883a
Mario <mario.alan.16180@gmail.com>
2023-03-29 20:12:57 +0400

    Update water_desalination_environment.py

--- src/environments/water_desalination_environment.py
+++ src/environments/water_desalination_environment.py
@@ -3,16 +3,20 @@ import numpy as np
 import pygame
 
 class WaterDesalinationEnvironment(gym.Environment):
-    def __init__(self, config):
-        self.action_space = {'Fossil Fuel': 0, 'Photovoltaic Panel': 1, 'Battery': 2}
-        self.state_space = {'Current height of brine water': 0, 'Current height of feed water': 1, 'Salt rejection': 2,
-                            'Recovery rate': 3, 'Current battery charge': 4, 'Osmotic pressure': 5,
-                            'Fossil fuel source - switch': 6, 'Photovoltaic panel source - switch': 7, 'Battery source - switch': 8,
-                            'Power': 9, }
+    Fossil_Fuel = 0
+    Photovoltaic_Panel = 1
+    Battery = 2
+    def __init__(self):
+        self.action_space_discrete = {'Fossil Fuel': 0, 'Photovoltaic Panel': 1, 'Battery': 2} # Selecting the energy source
+        self.action_space_continuous = {'Power': 0} # In Watts
+        self.state_space = {'Current height of feed water': 0, 'Current height of permeate water': 1,
+                            'Current battery charge': 2, 'Osmotic pressure': 3,
+                            'Fossil fuel source - switch': 4, 'Photovoltaic panel source - switch': 5, 'Battery source - switch': 6,
+                            'Power': 7}
         
         # Setting some limits in the environment
-        self.max_height_brine_water_tank = 10 # Meters, maybe
-        self.min_height_brine_water_tank = 0.5 # Meters, maybe
+        self.max_height_feed_water_tank = 10 # Meters, maybe
+        self.min_height_feed_water_tank = 0.5 # Meters, maybe
 
         self.max_height_permeate_water_tank = 10 # Meters, maybe
         self.min_height_permeate_water_tank = 0 # Meters, maybe
@@ -26,21 +30,44 @@ class WaterDesalinationEnvironment(gym.Environment):
         return state, reward, done, info
 
     def reset(self):
-        self.current_height_brine_water_tank = self.max_height_brine_water_tank / 2
-        self.current_height_permate_water_tank = 0
-        self.current_battery_charge = 0
-        self.current_energy_source = 0 # 0 = Fossil Fuel, 1 = Photovoltaic Panel, 2 = Battery
-        return []
+        self.reset_flag = True
+        state = state_calculation()
+        return state
 
     def reward_calculation(self):
         reward = 0
 
         return reward
 
-    def state_calculation(self):
+    def state_calculation(self, action):
+        if self.reset_flag == True:
+            self.current_height_feed_water_tank = self.max_height_brine_water_tank / 2
+            self.current_height_permate_water_tank = 0 # Meters, maybe
+            self.current_battery_charge = 0 # In percentage
+            self.current_energy_source = 0 # 0 = Fossil Fuel, 1 = Photovoltaic Panel, 2 = Battery
+            self.current_time = 0 # In minutes
+            self.current_power = 0 # In Watts
+            self.current_osmotic_pressure = 0 # In psi
+            self.reset_flag = False
+        elif action == Fossil_Fuel:
+            self.current_energy_source = 0
+        elif action == Photovoltaic_Panel:
+            self.current_energy_source = 1
+        elif action == Battery:
+            self.current_energy_source = 2
+        else:
+            raise ValueError("Received invalid action={} which is not part of the action space".format(action))
+        if reset_flag == False:
+            if self.current_energy_source == 0:
+                self.current_power = 1000 # In Watts
+            elif self.current_energy_source == 1:
+                self.current_power = 500
+            elif self.current_energy_source == 2:
+                self.current_power = 200
+            else:
+
         height_feed_water = 0
         osmotic_pressure = 0
-        return 
-    
+        return state    
     def check_done(self):
         pass
